6463e4ee397eba023c8b066552b077c61184af30
tbradsha <tbradsha@users.noreply.github.com>
2025-08-05 18:37:12 +0000

    Updated package versions for automattic/wpcomsh
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16758268808
    
    Upstream-Ref: Automattic/jetpack@50e27c392643e6493fee3327299474969010ee1d

--- vendor/jetpack-autoloader/class-latest-autoloader-guard.php
+++ vendor/jetpack-autoloader/class-latest-autoloader-guard.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp26841ac2064774301cbe06d174833bfc_wpcomshⓥ7_1_0_alpha\al5_0_9;
+namespace Automattic\Jetpack\Autoloader\jp26841ac2064774301cbe06d174833bfc_wpcomshⓥ7_1_0\al5_0_9;
 
  // phpcs:ignore
 
