c5bf50466332703ec3887d83014441cf89f50d6d
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-12-22 10:53:38 +0000

    Backport jetpack 15.4-a.5 changes (#46383)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    * Fix typo
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20429384995
    
    Upstream-Ref: Automattic/jetpack@d30c7fd4782e236aa8f4624b6c892b4bcefc14e0

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.18-alpha1766165482',
+      'ver' => '4.3.18',
     ),
     'jetpack-blaze' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-blaze',
-      'ver' => '0.26.22-alpha1766380116',
+      'ver' => '0.26.22',
     ),
     'jetpack-block-delimiter' => array(
       'path' => 'jetpack_vendor/automattic/block-delimiter',
@@ -22,7 +22,7 @@ return array(
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.14.7-alpha1766380116',
+      'ver' => '0.14.7',
     ),
     'jetpack-compat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-compat',
@@ -34,11 +34,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.19.13-alpha1766380116',
+      'ver' => '6.19.13',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.4-alpha1766380116',
+      'ver' => '0.4.4',
     ),
     'jetpack-google-analytics' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-google-analytics',
@@ -50,11 +50,11 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.13-alpha1766380116',
+      'ver' => '4.3.13',
     ),
     'jetpack-masterbar' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-masterbar',
-      'ver' => '0.27.6-alpha1766380116',
+      'ver' => '0.27.6',
     ),
     'jetpack-mu-wpcom' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-mu-wpcom',
@@ -66,7 +66,7 @@ return array(
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.9.0-alpha1766385163',
+      'ver' => '0.9.0',
     ),
     'jetpack-scheduled-updates' => array(
       'path' => 'jetpack_vendor/automattic/scheduled-updates',
@@ -82,11 +82,11 @@ return array(
     ),
     'jetpack-subscribers-dashboard' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-subscribers-dashboard',
-      'ver' => '0.4.12-alpha1766380116',
+      'ver' => '0.4.12',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.24.2-alpha1765963715',
+      'ver' => '4.24.2',
     ),
   ),
 );
