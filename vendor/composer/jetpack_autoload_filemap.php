7eba78fb185df0a8571c3e417aa94e5c53c3d229
anomiex <anomiex@users.noreply.github.com>
2026-06-25 19:46:16 +0000

    Update babel configs in preparation for Babel 8 (#49977)
    We're not quite ready to actually update to Babel 8, but we can go ahead
    with the things recommended at https://babeljs.io/docs/v8-migration.
    
    * `@babel/preset-env`: Set `bugfixes: true` to match Babel 8 behavior.
    * `@babel/preset-react`: Set `runtime: 'classic'` and `useSpread: true`
      for the few things using Preact. Babel 8 is changing the default
      runtime.
    * `@babel/preset-typescript`: Set `allowDeclareFields: true` to match
      Babel 8 behavior.
    * Babel 8 is moving `targets` from `@babel/preset-env` etc to the top
      level. Adjust the configs accordingly.
    * Set `targets` in `babel.config.js` where the project wasn't already
      setting a browserslist configuration, as babel's default is changing
      from "all browsers" to "browserslist defaults".
    * packages/search: Use `babel-plugin-polyfill-corejs3` directly, as
      `@babel/preset-env` is removing the `corejs` option in Babel 8.
    * plugins/inspect: Use standard babel config. The one it had seems like
      it was wrong (`targets: { node: 'current' }`?).
    * Clean up some unnecessary project-level babel configs.
      * Deleting various `*/.babelrc` seems to make no difference to
        anything.
      * Jetpack's `babel.config.js` had a bunch of complication that seems
        to make no difference.
    * Cleaned up unused babel deps in projects.
    
    As far as build output, the changes should be minor:
    `allowDeclareFields` explicitly sets some class fields to undefined, and
    `useSpread` has Babel use `...` in some React transforms. Other than
    that, things should generally be the same as before.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28195132223
    
    Upstream-Ref: Automattic/jetpack@a72d6efab88397f7e703e1992d7879de0a97c469

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -11,7 +11,7 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'7372b7fb88a9723cf5b76d456eb0b738' => array(
-		'version' => '8.7.4.0',
+		'version' => '8.7.5.0-alpha1782415648',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/actions.php'
 	),
 	'c8f84af3c24cff85f963d6926f4b2d42' => array(
