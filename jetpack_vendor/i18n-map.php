15af0a1277f3be1b72d173c25b65e15aabba5d02
Copons <Copons@users.noreply.github.com>
2026-07-17 16:49:45 +0000

    AI Launchpad: instrument the product analytics plane (#50606)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29596837717
    
    Upstream-Ref: Automattic/jetpack@0af82dea3c4ce05926ecdfa4ae2b01861e5b57b4

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -66,7 +66,7 @@ return array(
     ),
     'jetpack-mu-wpcom' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-mu-wpcom',
-      'ver' => '6.11.0-alpha1784303135',
+      'ver' => '6.11.0-alpha1784306195',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
