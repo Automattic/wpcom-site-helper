b524a816d9abf822f752b1fd9f1bf2a92e810aea
Nikschavan <Nikschavan@users.noreply.github.com>
2026-07-29 08:19:29 +0000

    Premium Analytics: remove Search Terms chart and show table deltas (#50799)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/30433263594
    
    Upstream-Ref: Automattic/jetpack@edee3cf79b1a68827976dbdbffa9a3710f7c359f

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -90,7 +90,7 @@ return array(
     ),
     'jetpack-premium-analytics-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-premium-analytics',
-      'ver' => '0.1.0-alpha1785309264',
+      'ver' => '0.1.0-alpha1785311540',
     ),
     'jetpack-rtc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-rtc',
